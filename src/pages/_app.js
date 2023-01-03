db90d4350293c87519348d6ca4bd2131474fbda6
Anush <anushshetty90@gmail.com>
2023-01-03 22:51:07 +0530

    T

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,20 +10,20 @@ export default function App({
       <Head >
   <meta name="viewport" content="width=device-width, initial-scale=1"/>
         
-  <title>8File - truly secure file sharing</title>
+  <title>8File - Truly secure file sharing</title>
   <meta name="description" content="Securely transfer files to users with GZIP compression and server-side AES-256 encryption. Upto 10GB for FREE!"/>
 
   <meta property="og:url" content="https://localhost"/>
   <meta property="og:type" content="website"/>
-  <meta property="og:title" content="8File - truly secure file sharing"/>
+  <meta property="og:title" content="8File - Truly secure file sharing"/>
   <meta property="og:description" content="Securely transfer files to users with GZIP compression and server-side AES-256 encryption. Upto 10GB for FREE!"/>
   <meta property="og:image" content="https://i.ibb.co/fxjK3WT/snapshot.png"/>
-  <meta property="og:site_name" content="8File - truly secure file sharing"/>
+  <meta property="og:site_name" content="8File - Truly secure file sharing"/>
 
   <meta name="twitter:card" content="summary_large_image"/>
   <meta property="twitter:domain" content="https://localhost"/>
   <meta property="twitter:url" content="https://localhost"/>
-  <meta name="twitter:title" content="8File - truly secure file sharing"/>
+  <meta name="twitter:title" content="8File - Truly secure file sharing"/>
   <meta name="twitter:description" content="Securely transfer files to users with GZIP compression and server-side AES-256 encryption. Upto 10GB for FREE!"/>
   <meta name="twitter:image" content="https://i.ibb.co/fxjK3WT/snapshot.png"/>
         
