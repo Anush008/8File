6e83942bcd4fa27524a823feb5017d48ff3692f9
Anush <anushshetty90@gmail.com>
2023-01-03 23:09:01 +0530

    Revert the next config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn.discordapp.com',
-        port: '',
-        pathname: '/avatars/**',
-      },
-    ],
-  },
 }
 
 module.exports = nextConfig
