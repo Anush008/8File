162caaa18217e38b5267280608e420f72ae3f2a7
Anush <anushshetty90@gmail.com>
2023-01-03 20:46:59 +0530

    Image tag instead of img

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.discordapp.com',
+        port: '',
+        pathname: '/avatars/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
