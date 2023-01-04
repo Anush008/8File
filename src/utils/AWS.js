1d2ce934a2ca2e530ec3a9617313a1e1318f2983
Anush <anushshetty90@gmail.com>
2023-01-05 00:15:06 +0530

    Update AWS.js

--- src/utils/AWS.js
+++ src/utils/AWS.js
@@ -1,8 +1,8 @@
 import {S3} from 'aws-sdk';
 const s3 = new S3({
   region: 'us-east-2',
-   accessKeyId: process.env.AWS_ACCESS_KEY,
-   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+   accessKeyId: process.env.AWS_ACCESS_KEYY,
+   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEYY
 });
 
 export default s3;
