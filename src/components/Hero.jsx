cfed6aaf3e60277c8156a18f692a8cfa46ad8044
Anush <anushshetty90@gmail.com>
2023-01-09 20:23:21 +0530

    Changed go to dashboard to upload file

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,9 +17,9 @@ const Hero = () => {
             }}
           /></span></h1>
           <p className="py-6">Securely transfer files to users with GZIP compression and server-side AES-256 encryption. <span class="underline--magical">Upto 10GB for FREE!</span></p>
-          <Link href={authtenticated ? "/dashboard" : "/api/auth/signin"}>
+          <Link href={authtenticated ? "/dashboard/upload" : "/api/auth/signin"}>
             <button className="w-full text-slate-500 border border-slate-500 hover:bg-slate-800 hover:text-white active:bg-slate-600 font-bold uppercase px-8 py-3 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
-              {authtenticated ? "Go to dashboard" : "Let Me In!"}
+              {authtenticated ? "Upload A File" : "Let Me In!"}
             </button>
           </Link>
         </div>
