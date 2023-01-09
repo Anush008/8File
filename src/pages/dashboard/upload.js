87e94a327af37a0edc78a1b9b2694fc70901dded
Anush <anushshetty90@gmail.com>
2023-01-09 20:25:19 +0530

    Lazy load on the upload page

--- src/pages/dashboard/upload.js
+++ src/pages/dashboard/upload.js
@@ -1,4 +1,8 @@
-import FileUploader from "../../components/FileUploader";
+import dynamic from 'next/dynamic'
+
+const FileUploader = dynamic(() => import("../../components/FileUploader"), {
+  loading: () => 'Loading...',
+});
 
 export default function Upload() {
   return (
