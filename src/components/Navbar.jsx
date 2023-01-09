795c13ead0972bfd101fa815b4e8e1e36d561acf
Anush <anushshetty90@gmail.com>
2023-01-09 20:24:06 +0530

    Added Upload file hyperlink in the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import { signOut, useSession, signIn } from "next-auth/react"
 import LogoSVG from '../components/LogoSVG';
 import formatBytes from '../utils/fileSizeParser';
+import Image from 'next/image';
 
 const Navbar = () => {
   const { data: session, status } = useSession();
@@ -13,12 +14,13 @@ const Navbar = () => {
         <span class="text-xl">File</span>
       </Link>
       <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
-        <Link href="/dashboard" class="font-medium mr-5 hover:text-gray-900">Dashboard</Link>
+        <Link href="/dashboard/upload" class="font-medium mr-5 hover:text-gray-900">Upload</Link>
+        <Link href="/dashboard" class="font-medium mr-5 hover:text-gray-900">Files</Link>
         <Link href="/#pricing" class="font-medium mr-5 hover:text-gray-900">Pricing</Link>
         <Link href="/#faq" class="font-medium mr-5 hover:text-gray-900">FAQs</Link>
       </nav>
       <div class="flex items-center space-x-4">
-        {authenticated && <img class="w-10 h-10 rounded-full" src={session?.user?.image || "/images/favicon.ico"} alt="User's profile picture" />}
+        {authenticated && <Image width="10" height="10" alt="User's avatar" class="w-10 h-10 rounded-full" src={session?.user?.image || "/images/favicon.ico"} />}
         <div class="font-medium ">
           <div>{authenticated ? (session.user.name || "Bruce Wayne") : <button class="btn" onClick={signIn}>Sign In</button>}</div>
           {authenticated && <div class="text-sm text-gray-500 ">{`Usage: ${formatBytes(session?.user?.storageUsed)} out of ${formatBytes(session?.user?.storageLimit)}`}</div>}
