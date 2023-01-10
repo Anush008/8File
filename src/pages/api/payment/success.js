cb104ce4ef483697b3a50808545fb85b6e5a690f
Anush <anushshetty90@gmail.com>
2023-01-10 22:04:45 +0530

    Pass a Date.now() + 30 object to premium expiry

--- src/pages/api/payment/success.js
+++ src/pages/api/payment/success.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
 
     if(req.body.status == "success"){
         console.log("HRE!")
-        const premiumExpiry = new Date(new Date().setDate(new Date().getDate() + 30)).toISOString();
+        const premiumExpiry = new Date(new Date().setDate(new Date().getDate() + 30));
         execute("UPDATE `users_addtional` set `premium` = 1, `storageLimit` = `storageLimit` * 10,`premiumExpiry` = ? where `email` = ?", [premiumExpiry, req.body.email]);
         res.redirect("/");
     }
