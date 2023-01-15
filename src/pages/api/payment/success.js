2a0b96b8d7fc640191b603109fea8d41512e22e2
Anush <anushshetty90@gmail.com>
2023-01-15 19:53:27 +0530

    Payments feed surl

--- src/pages/api/payment/success.js
+++ src/pages/api/payment/success.js
@@ -1,11 +1,12 @@
-import execute from "../../../utils/MySQL";
+import execute from "../../../utils/MySQL"
+
 export default async function handler(req, res) {
 
     if(req.body.status == "success"){
-        console.log("HRE!")
         const premiumExpiry = new Date(new Date().setDate(new Date().getDate() + 30));
-        execute("UPDATE `users_addtional` set `premium` = 1, `storageLimit` = `storageLimit` * 10,`premiumExpiry` = ? where `email` = ?", [premiumExpiry, req.body.email]);
-        res.redirect("/");
+        execute("UPDATE `users_addtional` set `premium` = 1, `storageLimit` = `storageLimit` * 10,`premiumExpiry` = ?, `txnid` = ? where `email` = ?", [premiumExpiry, req.body.txnid, req.body.email]);
+        execute("INSERT INTO `payments` values(?,?,?,?,?,?,?,?)", [req.body?.txnid, req.body?.bank_ref_num, req.body?.bankcode, req.body?.error, req.body?.mode, req.body?.status, req.body?.amount, req.body?.net_amount_debit ]);
+        res.redirect("/dashboard");
     }
-    else res.json(req.body)
+    else res.redirect("/")
 }
