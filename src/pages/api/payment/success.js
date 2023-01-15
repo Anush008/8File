82c65b6d64cfaa601b0c71e1d550d682f88650da
Anush <anushshetty90@gmail.com>
2023-01-15 20:00:36 +0530

    Redirect to homepage after a successful payment

--- src/pages/api/payment/success.js
+++ src/pages/api/payment/success.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
         const premiumExpiry = new Date(new Date().setDate(new Date().getDate() + 30));
         execute("UPDATE `users_addtional` set `premium` = 1, `storageLimit` = `storageLimit` * 10,`premiumExpiry` = ?, `txnid` = ? where `email` = ?", [premiumExpiry, req.body.txnid, req.body.email]);
         execute("INSERT INTO `payments` values(?,?,?,?,?,?,?,?)", [req.body?.txnid, req.body?.bank_ref_num, req.body?.bankcode, req.body?.error, req.body?.mode, req.body?.status, req.body?.amount, req.body?.net_amount_debit ]);
-        res.redirect("/dashboard");
+        res.redirect("/");
     }
     else res.redirect("/")
 }
