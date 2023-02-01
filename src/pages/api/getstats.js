9c8226e7def1f992bf6a4720a2dd1db4099aef94
Anush <anushshetty90@gmail.com>
2023-02-01 15:50:02 +0530

    Fixed formatting

--- src/pages/api/getstats.js
+++ src/pages/api/getstats.js
@@ -3,11 +3,11 @@ import execute from "../../utils/MySQL";
 export default async function handler(req, res) {
 const stats = {};
 const {DOWNLOADS, FILES, ENCRYPTED} = (await execute("SELECT SUM(`DOWNLOADS`) AS `DOWNLOADS`, COUNT(*) AS `FILES`, SUM(`SIZE`) AS `ENCRYPTED` FROM `files`"))[0];
-    const {USERS} = (await execute("SELECT COUNT(*) AS `USERS` FROM `users`"))[0];
-    stats.downloads = DOWNLOADS;
-    stats.files = FILES;
-    stats.encrypted = ENCRYPTED;
-    stats.users = USERS;
+const {USERS} = (await execute("SELECT COUNT(*) AS `USERS` FROM `users`"))[0];
+stats.downloads = DOWNLOADS;
+stats.files = FILES;
+stats.encrypted = ENCRYPTED;
+stats.users = USERS;
 res.setHeader('Cache-Control', 's-maxage=60,stale-while-revalidate=1296000');
 res.json(stats)
 }
